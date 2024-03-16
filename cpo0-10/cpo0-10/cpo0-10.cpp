06a530c5799e6d47eb7b35467312cf5640c5bc8f
tarrrre <rkakdrnfma@naver.com>
2024-03-16 12:13:00 +0900

    cpo 0

---
+++ cpo0-10/cpo0-10/cpo0-10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int
+main
+(
+)
+
+
+{
+    std
+        ::
+        cout
+        << 
+        "Hello, world!"
+        <<
+        std
+        ::
+        endl
+        ;
+  
+    return
+        0
+        ;
+}
